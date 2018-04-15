48f43f09e867a77bedc3b66b8656e67db71e1ece
Meghana Anvekar <maanvekar@gmail.com>
2018-04-15 21:02:45 +0530

    webhook for both

--- _firebase.py
+++ _firebase.py
@@ -51,9 +51,38 @@ def fetch_data(q):
 
 
 	return (final_ans)
+
+
+def fetch_questions(q):
+	lemmae_q = lemmatize(q)
+	print(lemmae_q)
+	category = 'misc'
 	
+	for x in categories:
+		for y in categories[x]:
+			for z in lemmae_q:	
+				if z == y:
+			    		category = x
+			    		print("match =>"+category)
+			    		break
+	u1 = category.decode('utf-8')
+	
+	docs = db.collection(u'questions').where(u'category', u'==',u1).get()
+	
+	qid = []
+	threshold = 0.4
+	for doc in docs:
+		w = Question.from_dict(doc.to_dict())
+		match_percent = is_match(w.question,q)
+		if match_percent >= threshold :
+			qid.append(w.qid)
+			
+	print(qid)
+	return qid
 
 
 if __name__ == '__main__':
     #fetch_data('When is the first sessional?')
-    fetch_data('Where is VNIT?')
+    fetch_questions('Where is VNIT?')
+    fetch_questions('When is the first sessional?')
+    
--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import json
 from flask import  request
 from flask import Flask
-from _firebase import fetch_data
+from flask import jsonify
+from _firebase import fetch_data, fetch_questions
 
 
 
@@ -10,15 +11,30 @@ app = Flask(__name__)
 
 
 @app.route('/', methods=['POST'])
-def webhook():
-	
+def webhook_dialogflow():
+	data = request.get_json()
+ 
+    	question = data['result']['resolvedQuery']
+ 	answer = fetch_data(question)
+ 	
 	res = {
-	"speech": "webhook working",
-	"displayText": "webhook ",
+	"speech": answer,
+	"displayText":answer,
 	"source": "webhookdata"
 	}
-	r = json.dumps(res)
-	return r
+	
+	return jsonify(res)
+	
+	
+@app.route('/questions', methods=['POST'])
+def webhook_android_app():
+	data = request.get_json()
+	question = data['question']
+	question_list = fetch_questions(question)
+	res = {
+	"question_list": question_list
+	}
+	return jsonify(res) 
 
 if __name__ == '__main__' :
     app.run(debug='true')
